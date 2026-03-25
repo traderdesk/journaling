88f059d5f770ad5980b2fdc35cbbc6de4880bcd7
traderdesk <alihaider2029@gmail.com>
2026-03-25 18:44:02 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,3 +4,4 @@ const supabaseKey = 'sb_publishable_q3HEazZmUVhQo9m8ZGsCXQ_qGo4IwID'
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
 console.log("Supabase connected:", supabase)
+bash
