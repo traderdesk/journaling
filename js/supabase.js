232e2a04c58fcd60aa82cdec1e3e874e44145fa2
traderdesk <alihaider2029@gmail.com>
2026-03-25 18:26:36 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,9 @@
 const supabaseUrl = 'https://vbxnoyariobfcetkuoyn.supabase.co'
 const supabaseKey = 'sb_publishable_q3HEazZmUVhQo9m8ZGsCXQ_qGo4IwID'
 
+const supabaseUrl = 'https://vbxnoyariobfcetkuoyn.supabase.co'
+const supabaseKey = 'sb_publishable_q3HEazZmUVhQo9m8ZGsCXQ_qGo4IwID'
+
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
+
+console.log("Supabase connected:", supabase)
