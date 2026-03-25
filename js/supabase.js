29611eeb901f34e0f4580935250744a58cd8b49a
traderdesk <alihaider2029@gmail.com>
2026-03-25 18:23:48 +0500

    supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://vbxnoyariobfcetkuoyn.supabase.co'
+const supabaseKey = 'sb_publishable_q3HEazZmUVhQo9m8ZGsCXQ_qGo4IwID'
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
